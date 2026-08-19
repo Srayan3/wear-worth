bf0da75f0ab95ee32612c69971d7a566267f1070
Shan Abrar Srayan <sa.srayan007@gmail.com>
2026-08-19 13:04:58 +0600

    V8

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -47,7 +47,7 @@
         </div>
         <div class="footer-bottom">
             <span>
-                Copyright © 2026 Wear Worth. All Rights Reserved. Designed and Developped by <a style="color: #c6c0b4;" href="https://www.instagram.com/srayan007/" target="_blank" rel="noopener">Shan Abrar Srayan</a>.
+                Copyright © 2026 Wear Worth. All Rights Reserved. Designed and Developped by <strong><a style="color: #e2cb9e;" href="https://www.instagram.com/srayan007/" target="_blank" rel="noopener">Shan Abrar Srayan</a></strong>.
             </span>
             <span>Cash on Delivery · bKash · Nagad</span>
         </div>
